1fcfea30abb1041e60652987884d5f83a6e7f059
bart <you@example.com>
2019-12-05 12:09:22 +0100

    poprawione

--- app/controllers/testowy_plugin1_controller.rb
+++ app/controllers/testowy_plugin1_controller.rb
@@ -3,31 +3,6 @@ class TestowyPlugin1Controller < ApplicationController
   
   def index
     @project = Project.find(params[:project_id])
-    allissues = @project.issues
-    
-    @issueslist = []
-    i = 0
-    allissues.each do |issue|
-      userdata = User.where(:id => issue.id)
-      userdata.each do |user|
-        @mail = user.mail      
-      end
-      
-      if issue.priority_id ==5
-        priority = 'immediate'
-      elsif issue.priority_id ==4
-        priority = 'urgent'
-      elsif issue.priority_id ==3
-        priority = 'high'
-      elsif issue.priority_id ==2
-        priority = 'normal'
-      elsif issue.priority_id ==1
-        priority = 'low'
-      end
-      
-      @issueslist[i] = {'id' => issue.id, 'subject' => issue.subject,  'mail'=> @mail, 'descript' => issue.description, 'priority'=> priority}  
-      i += 1
-    end
+    @allissues = @project.issues
   end
-  
 end
